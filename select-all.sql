29c9723baa475953365fb11c098b76d870d4ea24
Andy Whitfield <andy@andywhitfield.me.uk>
2012-07-16 21:01:26 +0100

    Removed old file.

--- select-all.sql
+++
@@ -1,9 +0,0 @@
-select * from UserAccount
-select * from UserAccountAuthentication
-select * from [Route]
-select * from RunLog
-
-/*
-delete from UserAccountAuthentication
-delete from UserAccount
-*/
